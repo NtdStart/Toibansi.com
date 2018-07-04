3b30045d53fa00d8322bbbe0208a94203f4efa79
ntd272 <ntd272@gmail.com>
2018-07-04 21:34:36 +0700

    update css left nar

--- PageChat/src/helpers/collapsiblesidebar.js
+++ PageChat/src/helpers/collapsiblesidebar.js
@@ -3,12 +3,12 @@ import $ from 'jquery';
 export function collapsiblesidebar() {
     $(document).ready(function () {
         $('#dismiss, .overlay').on('click', function () {
-            $('#navigation-right').removeClass('navigation-right-collapse');
+            $('#navigation-left').removeClass('navigation-left-collapse');
             $('.overlay').removeClass('active');
         });
         $('#dismiss').on('click', function () {
-            $('#navigation-right').addClass('navigation-right-collapse');
-            $('#navigation-right').addClass('active');
+            $('#navigation-left').addClass('navigation-left-collapse');
+            $('#navigation-left').addClass('active');
             $('.overlay').addClass('active');
             $('.collapse.in').toggleClass('in');
             $('a[aria-expanded=true]').attr('aria-expanded', 'false');
